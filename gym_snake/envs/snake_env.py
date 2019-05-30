f3c6fd5827db6212872cca1d25ead57af3104c6f
Cameron McCallie <mccallie.cameron@gmail.com>
2019-05-30 11:12:30 -0400

    gym setup

---
+++ gym_snake/envs/snake_env.py
@@ -0,0 +1,21 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+class SnakeEnv(gym.Env):
+  metadata = {'render.modes': ['human']}
+
+  def __init__(self):
+    return
+
+  def step(self, action):
+    return
+
+  def reset(self):
+    return
+
+  def render(self, mode='human'):
+    return
+
+  def close(self):
+    return
