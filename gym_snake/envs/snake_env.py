d677ad726db10f9fea4b350eaef068abfbcfbc94
Cameron McCallie <mccallie.cameron@gmail.com>
2019-06-03 11:55:03 -0400

    hacking away

--- gym_snake/envs/snake_env.py
+++ gym_snake/envs/snake_env.py
@@ -1,21 +1,39 @@
 import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
+import pyglet
+from gym import spaces
+# from gym.envs.classic_control import rendering
+# from gym.utils import seeding
+
+WINDOW_W = 1000
+WINDOW_H = 800
+
+
+class Window(pyglet.window.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 
 class SnakeEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
+    # metadata = {'render.modes': ['human']}
 
-  def __init__(self):
-    return
+    def __init__(self):
+        print('init')
+        # Left, up, right, down
+        self.action_space = spaces.Discrete(4)
 
-  def step(self, action):
-    return
+    def step(self, action):
+        return
+        # super().step(action)
 
-  def reset(self):
-    return
+    def reset(self):
+        print('reset')
 
-  def render(self, mode='human'):
-    return
+    def render(self):
+        print('render')
+        self.viewer = Window(WINDOW_W, WINDOW_H)
+        # self.viewer = rendering.SimpleImageViewer()
+        # self.viewer = rendering.Viewer(WINDOW_W, WINDOW_H)
+        # super().render()
 
-  def close(self):
-    return
+    def close(self):
+        print('close')
